cf906fc43680e7ea1bdde5f3e6d340bed5752c01
Amir Kavousian <kav.amir@gmail.com>
2025-07-21 17:37:50 -0700

    Install Vercel adopter so we can use output: server mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.devarmor.com",
   output: "server", // Enable server-side rendering for form handling
+  adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
